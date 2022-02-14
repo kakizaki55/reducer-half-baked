bfd8764c155d72f9c9738642fda9623443a4172a
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-14 12:04:54 -0800

    just did a refactor and it seem like it all works

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,10 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 
 const pinkRGB = `rgb(236, 72, 153)`
-
+const initialState = 0
 export default function Counter() {
-  const [count, setCount] = useState(0)
   const [currentColor, setCurrentColor] = useState(pinkRGB)
+  const countReducer = (count, action) => {
+    switch (action.type) {
+      case 'increment': {
+        return count + 1
+      }
+      case 'decrement': {
+        return count - 1
+      }
+      case 'reset': {
+        return 0
+      }
+      default: {
+        throw Error(`Unknown action: ${action.type}`)
+      }
+    }
+  }
+
+  const [count, dispatch] = useReducer(countReducer, initialState)
 
   useEffect(() => {
     if (count === 0) {
@@ -21,15 +38,22 @@ export default function Counter() {
   }, [count])
 
   const increment = () => {
-    setCount((prevState) => prevState + 1)
+    dispatch({
+      type: 'increment',
+      count,
+    })
   }
 
   const decrement = () => {
-    setCount((prevState) => prevState - 1)
+    dispatch({
+      type: 'decrement',
+    })
   }
 
   const reset = () => {
-    setCount(0)
+    dispatch({
+      type: 'reset',
+    })
   }
 
   return (
